629c86ecd276ce5537995ce285bcb62c57c31838
Rind Ovais <ovais@wiztech.pk>
2020-05-04 23:16:48 +0500

    multiple ui changes

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $(window).scroll(function() {    
+        var scroll = $(window).scrollTop();
+        if (scroll >= 160) {
+            $("header").addClass("height-0");
+            if (scroll >= 300) {
+                $("header").addClass("fixHeader");
+            }else{
+                $("header").removeClass("fixHeader");
+            }
+        }else{
+            $("header").removeClass("height-0");
+        }
+    });
+
+    var viewportWidth = $(window).width();
+    $(window).bind('resize, load',function() {
+        if(viewportWidth > 767){
+            console.log('hi')
+            $(".nav-item.dropdown").hover(function(){
+                $('.dropdown-toggle', this).trigger('click'); 
+            });
+        }
+    });
+})
